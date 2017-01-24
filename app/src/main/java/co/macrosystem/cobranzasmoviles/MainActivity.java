6d83ad50d58c6cef52b6770fbd024ba4087a55d1
Diego Armando Velez Peralta <si.macrosystem@gmail.com>
2017-01-24 02:11:22 -0500

    Validacion GPS funcional y validacion Campos del formulario de Login.

--- app/src/main/java/co/macrosystem/cobranzasmoviles/MainActivity.java
+++ app/src/main/java/co/macrosystem/cobranzasmoviles/MainActivity.java
@@ -1,5 +1,8 @@
 package co.macrosystem.cobranzasmoviles;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -9,55 +12,18 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.Manifest;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import android.Manifest;
-
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.location.LocationProvider;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -66,11 +32,10 @@ public class MainActivity extends AppCompatActivity {
     private TextInputLayout txtInputMail;
     private TextInputLayout txtInputPassw;
 
+    AlertDialog alert = null;
     Location location;
     LocationManager locationManager;
     LocationListener locationListener;
-    AlertDialog alert = null;
-    ObtenerWebService hiloconexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar;
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Version 1.0");
         setSupportActionBar(toolbar);
 
         txtMail = (TextInputEditText) findViewById(R.id.editTextEmail);
@@ -86,10 +52,74 @@ public class MainActivity extends AppCompatActivity {
         txtInputMail = (TextInputLayout) findViewById(R.id.text_input_layout_email);
         txtInputPassw = (TextInputLayout) findViewById(R.id.text_input_layout_pass);
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            AlertNoGps();
+        }
+
+    }
+
+    private void AlertNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("El sistema GPS esta desactivado, ¡activelo por favor!")
+                .setCancelable(false)
+                .setPositiveButton("Activar GPS", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                });
+
+        alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(alert != null)
+        {
+            alert.dismiss ();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            } else {
+                locationManager.removeUpdates(locationListener);
+            }
+        } else {
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                return;
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+            }
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        }
+
+
 
     }
 
     public void validar(View view) {
+
         /**
          * Valido si los campos estan sin digitar y en caso de que no mostrar mensaje de error obligatorio
          */
@@ -170,140 +200,9 @@ public class MainActivity extends AppCompatActivity {
         return isValid;
     }
 
-    private void AlertNoGps() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("El sistema GPS esta desactivado, desea activarlo?")
-                .setCancelable(false)
-                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialogInterface, @SuppressWarnings("unused") final int i) {
-                        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int i) {
-                        dialog.cancel();
-                    }
-                });
-        alert = builder.create();
-        alert.show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (alert != null) {
-            alert.dismiss();
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            } else {
-                locationManager.removeUpdates(locationListener);
-            }
-        } else {
-            locationManager.removeUpdates(locationListener);
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            } else {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-            }
-        } else {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        }
-    }
-
-    public void MostrarLocalizacion(Location loc) {
-        if (loc != null) {
-            hiloconexion = new ObtenerWebService();
-            hiloconexion.execute(String.valueOf(loc.getLatitude()), String.valueOf(loc.getLongitude()));
-        }
-    }
-
-    
 
 
-    public class ObtenerWebService extends AsyncTask<String, Integer, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            String cadena = "http://maps.googleapis.com/maps/api/geocode/json?latlng=";
-
-            //http://maps.googleapis.com/maps/api/geocode/json?latlng=38.404593,-0.529534&sensor=false
-            cadena = cadena + params[0];
-            cadena = cadena + ",";
-            cadena = cadena + params[1];
-            cadena = cadena + "&sensor=false";
 
 
-            String devuelve = "";
 
-            URL url = null; // Url de donde queremos obtener información
-            try {
-                url = new URL(cadena);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //Abrir la conexión
-                connection.setRequestProperty("User-Agent", "Mozilla/5.0" +
-                        " (Linux; Android 1.5; es-ES) Ejemplo HTTP");
-                //connection.setHeader("content-type", "application/json");
-
-                int respuesta = connection.getResponseCode();
-                StringBuilder result = new StringBuilder();
-
-                if (respuesta == HttpURLConnection.HTTP_OK) {
-
-
-                    InputStream in = new BufferedInputStream(connection.getInputStream());  // preparo la cadena de entrada
-
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));  // la introduzco en un BufferedReader
-
-                    // El siguiente proceso lo hago porque el JSONOBject necesita un String y tengo
-                    // que tranformar el BufferedReader a String. Esto lo hago a traves de un
-                    // StringBuilder.
-
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        result.append(line);        // Paso toda la entrada al StringBuilder
-                    }
-
-                    //Creamos un objeto JSONObject para poder acceder a los atributos (campos) del objeto.
-                    JSONObject respuestaJSON = new JSONObject(result.toString());   //Creo un JSONObject a partir del StringBuilder pasado a cadena
-                    //Accedemos al vector de resultados
-                    JSONArray resultJSON = respuestaJSON.getJSONArray("results");   // results es el nombre del campo en el JSON
-
-                    //Vamos obteniendo todos los campos que nos interesen.
-                    //En este caso obtenemos la primera dirección de los resultados.
-                    String direccion = "SIN DATOS PARA ESA LONGITUD Y LATITUD";
-                    if (resultJSON.length() > 0) {
-                        direccion = resultJSON.getJSONObject(0).getString("formatted_address");    // dentro del results pasamos a Objeto la seccion formated_address
-                    }
-                    devuelve = "Dirección: " + direccion;   // variable de salida que mandaré al onPostExecute para que actualice la UI
-
-                }
-
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return devuelve;
-        }
-
-    }
 }
